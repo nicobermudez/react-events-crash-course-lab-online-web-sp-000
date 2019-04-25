6f0dd5f7bb40ff7f09f14ceff22d77b394c6c3f0
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-25 00:03:38 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,7 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-     let x = screenX
+     let x = this.screenX
      let y = screenY
      drawChromeBoiAtCoords(x, y)
   }
